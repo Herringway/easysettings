cc891639543577f4016b894b913bff051cdafafa
Cameron Ross <elpenguino@gmail.com>
2019-12-09 22:03:31 -0400

    some style fixes

--- source/easysettings.d
+++ source/easysettings.d
@@ -1,3 +1,4 @@
+module easysettings;
 private import siryul;
 private import standardpaths;
 
@@ -18,10 +19,11 @@ auto loadSettings(T)(string name, string filename = settingsFilename) {
 	import std.file : exists;
 	import std.path : buildPath, setExtension;
 	auto paths = standardPaths(StandardPath.config).chain(["."]).map!(x => buildPath(x, name, filename.setExtension(settingsExtension))).filter!exists;
-	if (!paths.empty)
-		return fromFile!(T,settingsFormat)(paths.front);
-	else
+	if (!paths.empty) {
+		return fromFile!(T, settingsFormat)(paths.front);
+	} else {
 		saveSettings(T.init, name, filename);
+	}
 	return T.init;
 }
 ///
@@ -49,8 +51,9 @@ void saveSettings(T)(T data, string name, string filename = settingsFilename) {
 	import std.path : buildPath, setExtension;
 	import std.file : exists, mkdirRecurse;
 	string configPath = buildPath(writablePath(StandardPath.config), name);
-    if (!configPath.exists)
+    if (!configPath.exists) {
         mkdirRecurse(configPath);
+    }
 	data.toFile!settingsFormat(buildPath(configPath, filename.setExtension(settingsExtension)));
 }
 ///
@@ -74,10 +77,12 @@ void deleteSettings(string name, string filename = settingsFilename) {
 	import std.path : buildPath, dirName, setExtension;
 	import std.file : exists, remove, dirEntries, SpanMode, rmdir;
 	auto path = buildPath(writablePath(StandardPath.config), name, filename.setExtension(settingsExtension));
-	if (path.exists)
+	if (path.exists) {
 		remove(path);
-	if (path.dirName.exists && path.dirName.dirEntries(SpanMode.shallow).empty)
+	}
+	if (path.dirName.exists && path.dirName.dirEntries(SpanMode.shallow).empty) {
 		rmdir(path.dirName);
+	}
 }
 ///
 unittest {
