8f846b182e7057cda0486ec47eda699e2d5ebe5c
Cameron Ross <elpenguino@gmail.com>
2022-08-11 12:31:32 -0300

    allow formats other than yaml to be used

--- source/easysettings.d
+++ source/easysettings.d
@@ -3,8 +3,7 @@ private import siryul;
 private import standardpaths;
 
 private enum settingsFilename = "settings";
-private enum settingsExtension = "yml";
-private alias settingsFormat = YAML;
+private alias SettingsFormat = YAML;
 /**
  * Load settings. Will create settings file by default. Searches all system-wide
  * settings dirs as well as the user's settings dir, and loads the first file
@@ -14,21 +13,26 @@ private alias settingsFormat = YAML;
  * name = Subdirectory of settings dir to save config to. Created if nonexistent.
  * filename = The filename the settings will be loaded from.
  */
-auto loadSettings(T)(string name, string filename = settingsFilename) {
-	import std.algorithm : filter, map;
-	import std.range : chain;
+auto loadSettings(T, alias settingsFormat = SettingsFormat)(string name, string filename = settingsFilename) {
+	import std.algorithm : cartesianProduct, filter, map;
+	import std.conv : text;
+	import std.experimental.logger : tracef;
 	import std.file : exists;
-	import std.path : buildPath, setExtension;
-	auto paths = standardPaths(StandardPath.config).chain(["."]).map!(x => buildPath(x, name, filename.setExtension(settingsExtension))).filter!exists;
+	import std.path : chainPath, withExtension;
+	import std.range : chain, only;
+	auto searchPaths = standardPaths(StandardPath.config, name).chain(["."]).cartesianProduct(only(SettingsExtensions!settingsFormat)).map!(x => chainPath(x[0], filename.withExtension(x[1])));
+	auto paths = searchPaths.filter!exists;
+	tracef("Search paths: %s", searchPaths);
 	if (!paths.empty) {
-		return fromFile!(T, settingsFormat, DeSiryulize.optionalByDefault)(paths.front);
+		tracef("Loading settings from '%s'", paths.front);
+		return fromFile!(T, settingsFormat, DeSiryulize.optionalByDefault)(paths.front.text);
 	} else {
 		saveSettings(T.init, name, filename);
 	}
 	return T.init;
 }
 ///
-unittest {
+@system unittest {
 	struct Settings {
 		bool blah;
 		string text;
@@ -49,14 +53,14 @@ unittest {
  * name = The subdirectory of the settings dir to save the config to. Created if nonexistent.
  * filename = The filename the settings will be saved to.
  */
-void saveSettings(T)(T data, string name, string filename = settingsFilename) {
+void saveSettings(T, alias settingsFormat = SettingsFormat)(T data, string name, string filename = settingsFilename) {
 	import std.path : buildPath, setExtension;
 	import std.file : exists, mkdirRecurse;
-	string configPath = buildPath(writablePath(StandardPath.config), name);
+	string configPath = writablePath(StandardPath.config, name);
     if (!configPath.exists) {
         mkdirRecurse(configPath);
     }
-	data.toFile!settingsFormat(buildPath(configPath, filename.setExtension(settingsExtension)));
+	data.toFile!settingsFormat(buildPath(configPath, filename.setExtension(SettingsExtensions!settingsFormat[0])));
 }
 ///
 unittest {
@@ -76,10 +80,10 @@ unittest {
  * name = App name.
  * filename = The settings file that will be deleted.
  */
-void deleteSettings(string name, string filename = settingsFilename) {
+void deleteSettings(alias settingsFormat = SettingsFormat)(string name, string filename = settingsFilename) {
 	import std.path : buildPath, dirName, setExtension;
 	import std.file : exists, remove, dirEntries, SpanMode, rmdir;
-	auto path = buildPath(writablePath(StandardPath.config), name, filename.setExtension(settingsExtension));
+	auto path = buildPath(writablePath(StandardPath.config), name, filename.setExtension(SettingsExtensions!settingsFormat[0]));
 	if (path.exists) {
 		remove(path);
 	}
@@ -90,4 +94,13 @@ void deleteSettings(string name, string filename = settingsFilename) {
 ///
 unittest {
 	deleteSettings("testapp");
+}
+
+private template SettingsExtensions(T) {
+	import std.meta : AliasSeq;
+	static if (is(T == YAML)) {
+		alias SettingsExtensions = AliasSeq!(".yaml", ".yml");
+	} else static if (is(T == JSON)) {
+		alias SettingsExtensions = AliasSeq!(".json");
+	}
 }
