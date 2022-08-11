699a24692c79b528c132dd9d884bb9846317886e
Cameron Ross <elpenguino@gmail.com>
2022-08-11 20:27:03 -0300

    support subdirectories

--- source/easysettings.d
+++ source/easysettings.d
@@ -4,6 +4,26 @@ private import standardpaths;
 
 private enum settingsFilename = "settings";
 private alias SettingsFormat = YAML;
+
+/**
+ * Get a list of paths where settings files were found.
+ * Params:
+ * settingsForma = The serialization format used to save and load settings (YAML, JSON, etc)
+ * name = Subdirectory of settings dir to save config to. Created if nonexistent.
+ * filename = The filename the settings will be loaded from.
+ * subdir = The subdirectory that the settings will be loaded from.
+ */
+auto getSettingsPaths(alias settingsFormat = SettingsFormat)(string name, string subdir, string filename) {
+	import std.algorithm : cartesianProduct, filter, map;
+	import std.experimental.logger : tracef;
+	import std.file : exists;
+	import std.path : buildPath, chainPath, withExtension;
+	import std.range : chain, only;
+	auto searchPaths = standardPaths(StandardPath.config, buildPath(subdir, name)).chain(["."]).cartesianProduct(only(SettingsExtensions!settingsFormat)).map!(x => chainPath(x[0], filename.withExtension(x[1])));
+	tracef("Search paths: %s", searchPaths);
+	return searchPaths.filter!exists;
+}
+
 /**
  * Load settings. Will create settings file by default. Searches all system-wide
  * settings dirs as well as the user's settings dir, and loads the first file
@@ -12,17 +32,12 @@ private alias SettingsFormat = YAML;
  * T = Type of settings struct to load
  * name = Subdirectory of settings dir to save config to. Created if nonexistent.
  * filename = The filename the settings will be loaded from.
+ * subdir = The subdirectory that the settings will be loaded from.
  */
-auto loadSettings(T, alias settingsFormat = SettingsFormat)(string name, string filename = settingsFilename) {
-	import std.algorithm : cartesianProduct, filter, map;
+auto loadSettings(T, alias settingsFormat = SettingsFormat)(string name, string filename = settingsFilename, string subdir = "") {
 	import std.conv : text;
 	import std.experimental.logger : tracef;
-	import std.file : exists;
-	import std.path : chainPath, withExtension;
-	import std.range : chain, only;
-	auto searchPaths = standardPaths(StandardPath.config, name).chain(["."]).cartesianProduct(only(SettingsExtensions!settingsFormat)).map!(x => chainPath(x[0], filename.withExtension(x[1])));
-	auto paths = searchPaths.filter!exists;
-	tracef("Search paths: %s", searchPaths);
+	auto paths = getSettingsPaths!settingsFormat(name, subdir, filename);
 	if (!paths.empty) {
 		tracef("Loading settings from '%s'", paths.front);
 		return fromFile!(T, settingsFormat, DeSiryulize.optionalByDefault)(paths.front.text);
@@ -52,11 +67,12 @@ auto loadSettings(T, alias settingsFormat = SettingsFormat)(string name, string 
  * data = The data that will be saved to the settings file.
  * name = The subdirectory of the settings dir to save the config to. Created if nonexistent.
  * filename = The filename the settings will be saved to.
+ * subdir = The subdirectory that the settings will be saved in.
  */
-void saveSettings(T, alias settingsFormat = SettingsFormat)(T data, string name, string filename = settingsFilename) {
+void saveSettings(T, alias settingsFormat = SettingsFormat)(T data, string name, string filename = settingsFilename, string subdir = "") {
 	import std.path : buildPath, setExtension;
 	import std.file : exists, mkdirRecurse;
-	string configPath = writablePath(StandardPath.config, name);
+	string configPath = writablePath(StandardPath.config, buildPath(subdir, name));
     if (!configPath.exists) {
         mkdirRecurse(configPath);
     }
@@ -79,11 +95,12 @@ unittest {
  * Params:
  * name = App name.
  * filename = The settings file that will be deleted.
+ * subdir = Settings subdirectory to delete.
  */
-void deleteSettings(alias settingsFormat = SettingsFormat)(string name, string filename = settingsFilename) {
+void deleteSettings(alias settingsFormat = SettingsFormat)(string name, string filename = settingsFilename, string subdir = "") {
 	import std.path : buildPath, dirName, setExtension;
 	import std.file : exists, remove, dirEntries, SpanMode, rmdir;
-	auto path = buildPath(writablePath(StandardPath.config), name, filename.setExtension(SettingsExtensions!settingsFormat[0]));
+	auto path = buildPath(writablePath(StandardPath.config, buildPath(subdir, name)), filename.setExtension(SettingsExtensions!settingsFormat[0]));
 	if (path.exists) {
 		remove(path);
 	}
